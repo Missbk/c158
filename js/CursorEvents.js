427a458442507ebdce223c2ce192ff4fadf78328
Missbk <100034302+Missbk@users.noreply.github.com>
2023-07-17 18:55:47 -0600

    Add files via upload

---
+++ js/CursorEvents.js
@@ -0,0 +1,27 @@
+AFRAME.registerComponent("cursor-listener", {
+ 
+  init: function () {
+   
+  },
+  handlePlacesListState: function () {
+   
+
+
+      this.el.setAttribute("material", {
+        color: "#D76B30",
+        opacity: 1,
+      });
+    }
+  },
+  handleMouseEnterEvents: function () {
+    // Evento 'mouseenter' del cursor.
+    
+  handleMouseLeaveEvents: function () {
+    // Evento 'mouseleave' del cursor.
+    
+          });
+        }
+      }
+    });
+  },
+});
